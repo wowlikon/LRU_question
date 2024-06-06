23940e455cef491eb0d372b31b9448cba38dbe3c
wowlikon <73490630+wowlikon@users.noreply.github.com>
2024-06-06 13:48:09 +0300

    Update generate.py comments

--- generate.py
+++ generate.py
@@ -1,9 +1,11 @@
 import sys
 
+#Вычисление суммы
 summ = 0
 v = sys.argv[1]
 for c in v: summ += int(c)
 
+#Нахождение контрольного символа
 summ %= 11
 if summ == 10: summ = "X"
 else: summ = str(summ)
